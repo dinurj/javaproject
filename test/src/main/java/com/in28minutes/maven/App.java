369d5face523e4fc4a9bdf039ae73492ed1d4a57
dinurj <41896905+dinurj@users.noreply.github.com>
2018-07-31 14:31:29 +0530

    change simulate

--- test/src/main/java/com/in28minutes/maven/App.java
+++ test/src/main/java/com/in28minutes/maven/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World  Test Test!" );
+	     System.out.println( "Change to simulate only!" );
     }
 
 	public int calculateSomething() {
